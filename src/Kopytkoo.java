4d67e9b07e1fed61e04d2187f8ce0ae10aa0dbca
Gosia <Gosia@DESKTOP-768CS6O>
2019-03-30 20:22:27 +0100

    Start

---
+++ src/Kopytkoo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gosia
+ */
+public class Kopytkoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println ("To jest mój pierwszy program");
+        // TODO code application logic here
+    }
+    
+}
