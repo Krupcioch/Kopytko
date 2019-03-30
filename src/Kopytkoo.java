0f07e71c8a47fcda5a10a4f8e7ed052fa9dc2155
Gosia <Gosia@DESKTOP-768CS6O>
2019-03-30 23:45:21 +0100

    Start

--- src/Kopytkoo.java
+++ src/Kopytkoo.java
@@ -16,8 +16,49 @@ public class Kopytkoo {
     public static void main(String[] args) {
         System.out.println ("To jest mój pierwszy program");
         
-        System.out.println("to sie zmienilo");
-        // TODO code application logic here
+        String imie =  "Gosia";
+        String nazwisko = "Brocka";
+        long a = 124123123123L;
+        int b = 6;
+        long c;
+        byte liczba = 127;
+        c = a + b;
+        
+        System.out.println(c);
+        /*
+            Typy całkowite:
+        byte - 8 bit - od -128 do 127
+        short - 16 bit od -32,798 do 32,767
+        int- 31 bity - od -2 ^ 31 do 2 ^ 31- 1
+        long - 64 bit - od -2 ^ 63 do 2 ^ 63 - 1
+        
+        Znakowe:
+            char - 2 bajtowy
+            String - ciąg znaków
+        
+        Zmienne zmiennoprzecinkowe
+            float - 32 bit - 1,4 * 10 ^ -45 do 3,4 * 10 ^ 138
+            double - 64 bity - 4,9 * 10 ^ -324 do 1,8 * 10 ^ 308
+        
+        
+        */
+        
+        char znak = 'A';
+        float liczba2 = 4.57f; 
+        double liczba3 = 56.564564544;
+        
+                
+        int d= 5, e= 2;
+        double f= 12, g= 15;
+        int wynik1 = d / (int)g;
+        double wynik2 = d / g;
+       
+                
+        System.out.println(znak);
+        System.out.println(imie + " " + nazwisko);
+        System.out.println(liczba2);
+        System.out.println(liczba3);
+        System.out.println((double) d / e);   
     }
     
 }
