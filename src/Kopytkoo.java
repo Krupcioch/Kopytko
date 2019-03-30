fa587d455f3a16c660347625f8663a551df34396
Gosia <Gosia@DESKTOP-768CS6O>
2019-03-30 20:24:54 +0100

    Start

--- src/Kopytkoo.java
+++ src/Kopytkoo.java
@@ -15,6 +15,8 @@ public class Kopytkoo {
      */
     public static void main(String[] args) {
         System.out.println ("To jest mój pierwszy program");
+        
+        System.out.println("to sie zmienilo");
         // TODO code application logic here
     }
     
